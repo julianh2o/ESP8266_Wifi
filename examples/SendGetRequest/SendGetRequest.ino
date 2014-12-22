0eeef1f06146a279878c651b5b0a566af76fea7d
votton <eric.nees@gmail.com>
2014-12-22 14:25:36 -0500

    Update SendGetRequest.ino
    Just some comments.

--- examples/SendGetRequest/SendGetRequest.ino
+++ examples/SendGetRequest/SendGetRequest.ino
@@ -13,6 +13,7 @@ void setup() {
   wifi.reset();
   wifi.setMode(1);
   wifi.connect("insert_ssid_here","insert_password_here");
+  //should check to see if connect failed; print error
   out.print("Connected in ");
   out.print((millis() - time));
   out.println("ms");
@@ -21,12 +22,14 @@ void setup() {
 int count = 0;
 void loop() {
   String get = "GET /update?key=INSERT_THINGSPEAK_KEY&field1=";
+  //make they Key and Field into variables
   get += count;
   
   unsigned long time = millis();
   wifi.start("TCP","184.106.153.149",80);  //connect to thingspeak
   wifi.sendPayload(get);
   wifi.close();
+  //how do we know the above was successful?
   out.print("Connected in ");
   out.print((millis() - time));
   out.println("ms");
