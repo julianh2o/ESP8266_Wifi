6aa60fa000e6637508182db05a1fa20c106b5af8
Julian Hartline <julianh2o@gmail.com>
2014-12-28 19:23:53 -0800

    improving debugging drastically

--- ESP8266_Wifi.cpp
+++ ESP8266_Wifi.cpp
@@ -2,38 +2,101 @@
 
 ESP8266_Wifi::ESP8266_Wifi() {
     this->out = NULL;
+    this->timeout = 5000;
+    lineBuffer = "";
 }
 
 void ESP8266_Wifi::init() {
-  Serial.begin(115200);
+  ESP_SERIAL.begin(115200);
 }
 
+//TODO remove me?
 void ESP8266_Wifi::debugSerial(SoftwareSerial * serial) {
     this->out = serial;
 }
 
-void ESP8266_Wifi::send(String data) {
-  if (out != NULL) out->println("(ESP8266 Debug) SENDING: "+data);
-  Serial.print(data);
-  Serial.print("\r\n");
+void ESP8266_Wifi::flush() {
+  char c;
+  while(ESP_SERIAL.available()) {
+      c = ESP_SERIAL.read();
+      ESP8266_Wifi::handleCharacter(c);
+  }
 }
 
-boolean ESP8266_Wifi::waitFor(char * data) {
-  return Serial.find(data);
+void ESP8266_Wifi::send(String data) {
+  DEBUG_SERIAL.println("(ESP8266 Debug) SENDING: "+data);
+  ESP_SERIAL.print(data);
+  ESP_SERIAL.print("\r\n");
+}
+
+boolean ESP8266_Wifi::stringContains(String needle, String haystack) {
+    for (int i=0; i<needle.length()-haystack.length(); i++) {
+        String fragment = haystack.substring(i,i+needle.length());
+        DEBUG_SERIAL.print("check frag: ");
+        DEBUG_SERIAL.println(fragment);
+        if (fragment == needle) return true;
+    }
+    return false;
+}
+
+boolean ESP8266_Wifi::waitFor(char * waitFor) {
+  String dest = "";
+  return ESP8266_Wifi::waitForReturn(waitFor,dest);
+}
+
+void ESP8266_Wifi::handleCharacter(char c) {
+  lineBuffer += c;
+  if (c == '\n') {
+    DEBUG_SERIAL.print("(ESP8266 Output) ");
+    DEBUG_SERIAL.print(lineBuffer);
+    lineBuffer = "";
+  }
+}
+
+boolean ESP8266_Wifi::waitForReturn(char * waitFor, String &returnValue) {
+  long start = millis();
+  String buffer = "";
+  char c;
+  DEBUG_SERIAL.print("waiting for: ");
+  DEBUG_SERIAL.println(waitFor);
+  boolean found = false;
+  while(millis() - start < this->timeout) {
+    if (ESP_SERIAL.available()) {
+      c = ESP_SERIAL.read();
+      returnValue = lineBuffer;
+      ESP8266_Wifi::handleCharacter(c);
+      buffer += c;
+      //DEBUG_SERIAL.print("char: ");
+      //DEBUG_SERIAL.println(c);
+      if (c == '\n' && found) {
+          DEBUG_SERIAL.print("FOUND: ");
+          DEBUG_SERIAL.println(waitFor);
+          return true;
+      }
+
+
+      if (buffer.endsWith(waitFor)) {
+          found = true;
+      }
+    }
+  }
+  DEBUG_SERIAL.println("ERROR: WaitFor timeout out.");
+  return found;
 }
 
 boolean ESP8266_Wifi::reset() {
   ESP8266_Wifi::send("AT+RST");
   boolean success = ESP8266_Wifi::waitFor("ready");
-  Serial.flush();
+  ESP8266_Wifi::flush();
   return success;
 }
 
 int ESP8266_Wifi::getMode() {
   ESP8266_Wifi::send("AT+CWMODE?");
-  if (ESP8266_Wifi::waitFor("+CWMODE:")) {
-    int mode = Serial.parseInt();
-    Serial.flush();
+  String line = "";
+  if (ESP8266_Wifi::waitForReturn("+CWMODE:",line)) {
+    int mode = line.substring(line.indexOf('+')+8).toInt();
+    ESP8266_Wifi::flush();
     return mode;
   } else {
     return -1;
@@ -49,20 +112,20 @@ void ESP8266_Wifi::setMode(int mode) {
   cmd += mode;
   ESP8266_Wifi::send(cmd);
   ESP8266_Wifi::waitFor("OK");
-  Serial.flush();
+  ESP_SERIAL.flush();
 }
 
 boolean ESP8266_Wifi::connect(String ssid, String password) {
   ESP8266_Wifi::send("AT+CWJAP=\""+ssid+"\",\""+password+"\"");
   boolean success = ESP8266_Wifi::waitFor("OK");
-  Serial.flush();
+  ESP_SERIAL.flush();
   return success;
 }
 
 boolean ESP8266_Wifi::start(String protocol, String ip, int port) {
   ESP8266_Wifi::send("AT+CIPSTART=\""+protocol+"\",\""+ip+"\","+port);
   boolean success = ESP8266_Wifi::waitFor("Linked");
-  Serial.flush();
+  ESP_SERIAL.flush();
   return success;
 }
 
--- ESP8266_Wifi.h
+++ ESP8266_Wifi.h
@@ -4,23 +4,39 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 
+#define DEBUG_SERIAL Serial
+#define ESP_SERIAL Serial1
+
 class ESP8266_Wifi {
 public:
+    static const int MODE_STA = 1;
+    static const int MODE_AP = 2;
+    static const int MODE_BOTH = 2;
+
     ESP8266_Wifi();
     void init();
+    boolean connect(String ssid, String password);
+    boolean get(String ip, String getString, String &result);
+
     void debugSerial(SoftwareSerial * serial);
-    void send(String data);
-    boolean waitFor(char * data);
     boolean reset();
     int getMode();
     void setMode(int mode);
-    boolean connect(String ssid, String password);
     boolean start(String protocol, String ip, int port);
     boolean sendPayload(String get);
     boolean close();
 
 private:
     SoftwareSerial * out;
+    long timeout;
+    String lineBuffer;
+
+    void send(String data);
+    void flush();
+    boolean waitFor(char * data);
+    boolean waitForReturn(char * waitFor, String &returnValue);
+    void handleCharacter(char c);
+    boolean stringContains(String needle, String haystack);
 };
 
 #endif
